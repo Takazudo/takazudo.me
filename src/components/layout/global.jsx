7749489b49dc686ae78e1c2113895ecfbb617ca1
Takeshi Takatsudo <takazudo@gmail.com>
2022-02-06 14:13:20 +0900

    fixed footer layout

--- src/components/layout/global.jsx
+++ src/components/layout/global.jsx
@@ -9,24 +9,32 @@ import { Footer } from "../global/footer";
 const GlobalLayout = ({ children, path }) => {
   return (
     <>
-      <Header />
-      <AnimatePresence exitBeforeEnter>
-        <motion.main
-          key={path}
-          initial={{ opacity: 0, y: -10, ease: "easeInOut" }}
-          animate={{ opacity: 1, y: 0, ease: "easeInOut" }}
-          exit={{ opacity: 0 }}
-          transition={{
-            type: "spring",
-            mass: 0.35,
-            stiffness: 75,
-            duration: 0.1,
-          }}
-        >
-          {children}
-          <Footer />
-        </motion.main>
-      </AnimatePresence>
+      <div className="min-h-screen relative">
+        <Header />
+        <div className="pb-[180px]">
+          <AnimatePresence exitBeforeEnter>
+            <motion.main
+              key={path}
+              initial={{ opacity: 0, y: -10, ease: "easeInOut" }}
+              animate={{ opacity: 1, y: 0, ease: "easeInOut" }}
+              exit={{ opacity: 0 }}
+              transition={{
+                type: "spring",
+                mass: 0.35,
+                stiffness: 75,
+                duration: 0.1,
+              }}
+            >
+              {children}
+            </motion.main>
+          </AnimatePresence>
+        </div>
+        <div className="absolute right-0 bottom-0 left-0 h-[200px] md:h-[220px]">
+          <div className="absolute right-0 bottom-0 left-0">
+            <Footer />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
