d6d7a8f9110afabe5f984c6350488ce21d92af3e
Takeshi Takatsudo <takazudo@gmail.com>
2022-02-11 16:53:55 +0900

    click to release hash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,26 @@ import * as React from "react";
 import "./src/styles/global.css";
 import { GlobalLayout } from "./src/components/layout/global";
 
+// click to release hash
+const enableClickToReleaseHash = () => {
+  if (!window) return;
+  window.addEventListener("click", (e) => {
+    if (!window.location.hash) return;
+    window.history.replaceState(
+      null,
+      window.document.title,
+      window.location.pathname
+    );
+  });
+};
+
+const onInitialClientRender = () => {
+  enableClickToReleaseHash();
+};
+
 const wrapPageElement = ({ element, props }) => {
+  enableClickToReleaseHash();
   return <GlobalLayout {...props}>{element}</GlobalLayout>;
 };
 
-export { wrapPageElement };
+export { onInitialClientRender, wrapPageElement };
